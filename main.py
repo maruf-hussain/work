3e8cdc7ebb48be119d896f1f6f1f015abebfe946
maruf-hussain <engmarufhussain@gmail.com>
2025-05-25 15:52:32 +0600

    grade system add

---
+++ main.py
@@ -0,0 +1,26 @@
+
+# Pythont genarel math............
+# num1=50
+# num2=40
+# sum=num1+num2
+# print(sum)
+
+# Python Condition.................
+age=20
+# if age>=20:
+#     print("you are adult")
+# else: print("you are chile")
+
+mark=90
+if mark<=100 and mark>=80:
+    print("Your Grade A+")
+elif mark<=80 and mark>=70:
+    print("A")
+elif mark<=70 and mark>=60:
+    print("A-")
+elif mark<=60 and mark>=50:
+    print("B")
+elif mark<=50 and mark>=40:
+    print("C")
+elif mark<=40:
+    print("Faled")
