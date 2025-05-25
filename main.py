57826b9b0ea57f8c59b71538f0f59fed8ef03947
maruf-hussain <engmarufhussain@gmail.com>
2025-05-26 01:02:05 +0600

    for loop in dictonary

--- main.py
+++ main.py
@@ -6,23 +6,38 @@
 # print(sum)
 
 # Python Condition.................
-age=20
+# age=20
 # if age>=20:
 #     print("you are adult")
 # else: print("you are chile")
+#
+# mark=30
+# if mark<=100 and mark>=80:
+#     print("Your Grade A+")
+# elif mark<=80 and mark>=70:
+#     print("A")
+# elif mark<=70 and mark>=60:
+#     print("A-")
+# elif mark<=60 and mark>=50:
+#     print("B")
+# elif mark<=50 and mark>=40:
+#     print("C")
+# elif mark<=40 and mark>=33:
+#     print("D")
+# else:
+#     print("F")
 
-mark=30
-if mark<=100 and mark>=80:
-    print("Your Grade A+")
-elif mark<=80 and mark>=70:
-    print("A")
-elif mark<=70 and mark>=60:
-    print("A-")
-elif mark<=60 and mark>=50:
-    print("B")
-elif mark<=50 and mark>=40:
-    print("C")
-elif mark<=40 and mark>=33:
-    print("D")
-else:
-    print("F")
+
+# For Loop..................................
+# List item......................
+# Citis = ['dhaka','kolkata','bankok','paris']
+# for City in Citis:
+#     print(City)
+#
+#     for item in range(101):
+#         print(item)
+
+# dictonary....................................
+examResult = {'bangla': 95, 'math': 85, 'english':75}
+for subject,marks in examResult.items():
+    print(subject,marks)
